234e277677c86dbe7728e8c7aeb9dbcf70a168e8
Yash840 <yashjawle440@gmail.com>
2025-10-23 21:46:11 +0530

    delete: dotenv loading from main class

--- src/main/java/org/cross/cauth/CrossAuthApplication.java
+++ src/main/java/org/cross/cauth/CrossAuthApplication.java
@@ -1,6 +1,5 @@
 package org.cross.cauth;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrossAuthApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries()
-                .forEach(dotenvEntry ->
-                System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue())
-                );
 
 		SpringApplication.run(CrossAuthApplication.class, args);
 	}
